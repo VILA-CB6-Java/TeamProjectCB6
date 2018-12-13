c361888297879db76d9cef6d082586d8e4f2cc56
IraPap <8141539+IraPap@users.noreply.github.com>
2018-12-13 10:39:57 +0200

    Add files via upload

---
+++ config-service/src/main/java/com/cb6/project/configservice/JwtConfig.java
@@ -0,0 +1,48 @@
+package com.cb6.project.configservice;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class JwtConfig {
+	
+	public JwtConfig() {
+		
+	}
+	
+	@Value("${security.jwt.uri:/auth/**}")
+    private String Uri;
+
+    @Value("${security.jwt.header:Authorization}")
+    private String header;
+
+    @Value("${security.jwt.prefix:Bearer }")
+    private String prefix;
+
+    @Value("${security.jwt.expiration:#{24*60*60}}")
+    private int expiration;
+
+    @Value("${security.jwt.secret:JwtSecretKey}")
+    private String secret;
+
+	public String getUri() {
+		return Uri;
+	}
+
+	public String getHeader() {
+		return header;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public int getExpiration() {
+		return expiration;
+	}
+
+	public String getSecret() {
+		return secret;
+	}
+    
+    
+
+}
