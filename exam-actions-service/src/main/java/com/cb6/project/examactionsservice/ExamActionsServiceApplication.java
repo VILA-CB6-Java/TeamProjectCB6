b9c3b04e2c3f813f3e8c9ff37187722cb9136595
Vasileios Sotiropoulos <bill_methcs@yahoo.gr>
2018-12-14 11:16:00 +0200

    no message

---
+++ exam-actions-service/src/main/java/com/cb6/project/examactionsservice/ExamActionsServiceApplication.java
@@ -0,0 +1,14 @@
+package com.cb6.project.examactionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamActionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamActionsServiceApplication.class, args);
+	}
+
+}
+
