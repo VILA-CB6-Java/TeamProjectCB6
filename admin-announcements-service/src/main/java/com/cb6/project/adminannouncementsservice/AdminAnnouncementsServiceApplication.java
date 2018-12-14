31ca67c03105b70c8938cbf1daf9d55ab0c3848d
Vasileios Sotiropoulos <bill_methcs@yahoo.gr>
2018-12-14 15:53:43 +0200

    no message

--- admin-announcements-service/src/main/java/com/cb6/project/adminannouncementsservice/AdminAnnouncementsServiceApplication.java
+++ admin-announcements-service/src/main/java/com/cb6/project/adminannouncementsservice/AdminAnnouncementsServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
