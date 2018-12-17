025a823206989c4820e01fcccfccbc46de728c8f
Vasileios Sotiropoulos <bill_methcs@yahoo.gr>
2018-12-17 16:40:20 +0200

    no message

--- exam-actions-service/src/main/java/com/cb6/project/examactionsservice/ExamActionsServiceApplication.java
+++ exam-actions-service/src/main/java/com/cb6/project/examactionsservice/ExamActionsServiceApplication.java
@@ -2,8 +2,10 @@ package com.cb6.project.examactionsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ExamActionsServiceApplication {
 
 	public static void main(String[] args) {
--- user-actions-service/src/main/java/com/cb6/project/useractionsservice/UserActionsServiceApplication.java
+++ user-actions-service/src/main/java/com/cb6/project/useractionsservice/UserActionsServiceApplication.java
@@ -2,8 +2,10 @@ package com.cb6.project.useractionsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserActionsServiceApplication {
 
 	public static void main(String[] args) {
