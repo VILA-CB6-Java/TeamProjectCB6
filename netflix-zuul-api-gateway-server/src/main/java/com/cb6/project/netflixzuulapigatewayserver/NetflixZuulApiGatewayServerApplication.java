e8886d44e5cd996ebfd0324bf858185cba5af596
Vasileios Sotiropoulos <bill_methcs@yahoo.gr>
2018-12-20 09:54:04 +0200

    no message

--- netflix-zuul-api-gateway-server/src/main/java/com/cb6/project/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ netflix-zuul-api-gateway-server/src/main/java/com/cb6/project/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +15,7 @@ import brave.sampler.Sampler;
 @Configuration
 @EnableAutoConfiguration(exclude={RabbitAutoConfiguration.class})
 @EnableZuulProxy
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class NetflixZuulApiGatewayServerApplication {
 
